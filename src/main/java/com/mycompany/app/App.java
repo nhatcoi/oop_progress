acc7471fa7cd78ba4af7333114384c41f98fff96
Nhật Côi <92137849+nhatcoi@users.noreply.github.com>
2024-02-22 14:49:19 +0000

    Assignment is Tricky Part 2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,14 +10,14 @@ public class App {
         public int i;
     }
        
-
     public static void main(String[] args) {
         Number n1 = new Number();
         Number n2 = new Number();
         n1.i = 2;
         n2.i = 5;
-        n1.i = n2.i;
+        n1 = n2;
         n2.i = 10;
-        System.out.println(n1.i + "  " + n2.i);
+        n1.i = 20;
+        System.out.println(n1.i + " " + n2.i);
     }
 }
