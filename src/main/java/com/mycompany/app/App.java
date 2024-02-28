b052477075d917ed206dc96c35a70717471f7362
Nhật Côi <23010887@st.phenikaa-uni.edu.vn>
2024-02-28 17:55:57 +0700

    Committing changes before switching branches

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,8 @@
 package com.mycompany.app;
 
-import com.mycompany.app.week2.Book;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
-
-        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
-        book.printBookDetails();
 
     }
 }
