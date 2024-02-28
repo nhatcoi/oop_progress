39b41dd68b9b82d5070f37803d717766958aa961
Nhật Côi <23010887@st.phenikaa-uni.edu.vn>
2024-02-28 17:29:47 +0700

    ex week 1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,13 @@
 package com.mycompany.app;
 
-import com.mycompany.app.week1.E1;
+import com.mycompany.app.week2.Book;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
-        //
 
-        E1 e1 = new E1();
-
-        e1.Ex1();
+        Book book = new Book("The Alchemist", "Paulo Coelho", 1988);
+        book.printBookDetails();
 
     }
 }
