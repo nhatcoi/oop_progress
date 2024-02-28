97b151c56b6dbc53184663fbaa75c5dbc4a6378a
Nhật Côi <23010887@st.phenikaa-uni.edu.vn>
2024-02-28 16:48:08 +0700

    treat

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,16 +1,15 @@
 package com.mycompany.app;
 
 import com.mycompany.app.week1.E1;
-import com.mycompany.app.week1.E10;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
         //
 
-        E10 e10 = new E10();
+        E1 e1 = new E1();
 
-        e10.Ex10();
+        e1.Ex1();
 
     }
 }
