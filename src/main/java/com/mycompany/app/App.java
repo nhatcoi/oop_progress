156753caada612cdfb559969a8eb3c5024b33db0
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-29 07:55:58 +0000

    Fix revoke and call class to main. General comment: Good Start! Co^

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,13 @@
 package com.mycompany.app;
-import com.mycompany.app.week1.E1
+import com.mycompany.app.week1.E1;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
         //call or revoke the work of each task in  here
+        // for example, revoke class E1
+        E1 e = new E1();
+        e.Ex1();
+
     }
 }
