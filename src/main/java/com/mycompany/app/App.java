95527b40134082f3eccf6990225dd83609811776
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2024-02-29 14:48:39 +0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,5 +3,6 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
+        //call or revoke the work of each task in  here
     }
 }
