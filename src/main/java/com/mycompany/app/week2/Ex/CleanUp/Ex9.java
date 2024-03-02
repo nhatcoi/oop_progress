88678d5a9e89d043d8109db9d5b1c008f82883b0
ndviet0303 <ndviet.dev@gmail.com>
2024-03-02 15:28:34 +0700

    E9 cleanup

---
+++ src/main/java/com/mycompany/app/week2/Ex/CleanUp/Ex9.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.week2.Ex.CleanUp;
+
+public class Ex9 {
+    private int val1;
+
+    public Ex9() {
+        val1 = 0;
+    }
+
+    public Ex9(int val1) {
+        this.val1 = val1;
+    }
+}
