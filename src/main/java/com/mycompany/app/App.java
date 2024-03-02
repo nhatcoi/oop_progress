088d3f62411e76cfaa6c16da3baa98753215dd79
ndviet0303 <ndviet.dev@gmail.com>
2024-03-02 14:33:20 +0700

    E5 Update imports and add new functionality

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,25 @@
 package com.mycompany.app;
 
 import com.mycompany.app.Debug.Debug;
-import com.mycompany.app.week2.Ex.E4;
+import com.mycompany.app.week2.Ex.E5;
+
 public class App {
     public static void main(String[] args) {
-        Debug debug= new Debug();
+        Debug debug = new Debug();
         System.out.println("Group 17");
 
-            E4.Dog dog = new E4.Dog("Fido", 50);
-            //Debug.debug(dog.name); cant not call this because use protected
-            debug.debug(dog.getName()); // can call this because use getter
+        E5 e5 = new E5();
+        debug.debug(e5.publicField + ""); // can call this because public
+        e5.publicMethod();
+
+        // debug.debug(e5.privateField + ""); // cant call this because private
+        // e5.privateMethod();
+
+        // debug.debug(e5.protectedField + ""); // cant call this because protected
+        // e5.protectedField();
+
+        // E4.Dog dog = new E4.Dog("Fido", 50);
+        // //Debug.debug(dog.name); cant not call this because use protected
+        // debug.debug(dog.getName()); // can call this because use getter
     }
 }
