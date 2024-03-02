a8c2cfa72e365d50822422e8d13146aa6d6b832c
ndviet0303 <ndviet.dev@gmail.com>
2024-03-02 14:23:20 +0700

    protected

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,13 @@
 package com.mycompany.app;
 
+import com.mycompany.app.Debug.Debug;
+import com.mycompany.app.week2.Ex.E4;
 public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
 
-        System.out.println("Week 2");
+            E4.Dog dog = new E4.Dog("Fido", 50);
+            //Debug.debug(dog.name); cant not call this because use protected
+
     }
 }
