22506390f3e62b33134f415e430d56cdecd171b5
ndviet0303 <ndviet.dev@gmail.com>
2024-03-02 15:14:53 +0700

    Ex4 Cleanup

--- src/main/java/com/mycompany/app/week2/Ex/CleanUp/Ex3.java
+++ src/main/java/com/mycompany/app/week2/Ex/CleanUp/Ex3.java
@@ -3,7 +3,13 @@ package com.mycompany.app.week2.Ex.CleanUp;
 import com.mycompany.app.Debug.Debug;
 
 public class Ex3 {
+    Debug debug = new Debug();
+
     public Ex3() {
-        new Debug().debug("Ex3");
+        debug.debug("Ex3");
+    }
+
+    public Ex3(String messageString) {
+        debug.debug(messageString);
     }
 }
