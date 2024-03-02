bdf53b16dfde9048b8c5317c42cd8942919598ff
ndviet0303 <ndviet.dev@gmail.com>
2024-03-02 20:10:01 +0700

    Reuse E9

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,7 +2,8 @@ package com.mycompany.app;
 
 import com.mycompany.app.Debug.Debug;
 
-import com.mycompany.app.week2.Ex.ReUse.Ex7;
+import com.mycompany.app.week2.Ex.ReUse.Ex9;
+import com.mycompany.app.week2.Ex.ReUse.Ex9.Stem;
 
 public class App {
     static Debug debug = new Debug();
@@ -10,7 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Group 17");
 
-        Ex7 ex = new Ex7();
-        Ex7.C test = ex.new C("123", "Hello");
+        Stem stem = new Ex9().new Stem();
     }
 }
