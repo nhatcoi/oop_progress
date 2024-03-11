60326aa578b97c14420e3321946046dbd11723ad
ndviet0303 <ndviet.work@gmail.com>
2024-03-12 00:11:57 +0700

    TreeView FixBug

--- src/main/java/com/mycompany/app/final_project/view/TreeView.java
+++ src/main/java/com/mycompany/app/final_project/view/TreeView.java
@@ -9,7 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.File;
-import java.util.Objects;
+import java.util.*;
 
 public class TreeView extends Home {
     public TreeView() {
@@ -26,15 +26,15 @@ public class TreeView extends Home {
         int pos = 0;
         for (File file : fl) {
             if (file.isHidden()) {
-                continue; // Bỏ qua các tệp ẩn
+                continue;
             }
             if (file.isDirectory()) {
-                A[pos] = new TreeItem<>(file.getName(), new ImageView(new Image(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream("img/folder.png")))));
+                A[pos] = new TreeItem<>(file.getName(), new ImageView(new Image((ClassLoader.getSystemResourceAsStream("img/folder.png")))));
                 try {
                     A[pos].getChildren().addAll(TreeCreate(file));
                     pos++;
                 } catch (Exception x) {
-                    System.out.println("treecreate tại " + file.getAbsolutePath() + " " + x.getMessage());
+                    System.out.println("treecreate " + file.getAbsolutePath() + " " + x.getMessage());
                 }
             }
         }
@@ -51,25 +51,52 @@ public class TreeView extends Home {
             return FindAbsolutePath(item.getParent(), dir);
         }
     }
-
+    private Set<String> loadedDirectories = new HashSet<>();
     @Override
-    public void CreateTreeView(javafx.scene.control.TreeView<String> treeview) {
-        File[] sysroots = File.listRoots();
-        TreeItem<String> ThisPc = new TreeItem<>("This PC", new ImageView(new Image(ClassLoader.getSystemResourceAsStream("img/pc.png"))));
-        TreeItem<String>[] drives = new TreeItem[sysroots.length];
-        for (int i = 0; i < sysroots.length; i++) {
-            drives[i] = new TreeItem<>(sysroots[i].getAbsolutePath(), new ImageView(new Image(ClassLoader.getSystemResourceAsStream("img/drive.png"))));
-            try {
-                drives[i].getChildren().addAll(TreeCreate(sysroots[i]));
-            } catch (NullPointerException x) {
-                System.out.println("Exeption x detected: " + x.fillInStackTrace() + drives[i].toString());
-            } finally {
-                ThisPc.getChildren().add(drives[i]);
+    public void CreateTreeView(javafx.scene.control.TreeView<String> treeView) {
+        TreeItem<String> rootNode = new TreeItem<>("This PC", new ImageView(new Image(ClassLoader.getSystemResourceAsStream("img/pc.png"))));
+        rootNode.setExpanded(true);
+
+        treeView.setRoot(rootNode);
+
+        treeView.setOnMouseClicked(event -> {
+            TreeItem<String> selectedItem = treeView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null && selectedItem.getChildren().isEmpty()) {
+                String fullPath = getFullPath(selectedItem);
+                loadDirectory(selectedItem, new File(fullPath));
             }
+        });
+
+        File[] sysroots = File.listRoots();
+        for (File sysroot : sysroots) {
+            TreeItem<String> driveNode = new TreeItem<>(sysroot.getAbsolutePath(), new ImageView(new Image(ClassLoader.getSystemResourceAsStream("img/drive.png"))));
+            rootNode.getChildren().add(driveNode);
         }
-        treeview.setRoot(ThisPc);
     }
 
+    private String getFullPath(TreeItem<String> item) {
+        StringBuilder fullPath = new StringBuilder(item.getValue());
+        TreeItem<String> parent = item.getParent();
+        while (parent != null && parent.getParent() != null) {
+            fullPath.insert(0, parent.getValue() + File.separator);
+            parent = parent.getParent();
+        }
+        return fullPath.toString();
+    }
+
+    private void loadDirectory(TreeItem<String> parentItem, File directory) {
+        if (directory.exists() && directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        TreeItem<String> directoryNode = new TreeItem<>(file.getName(),new ImageView(new Image(ClassLoader.getSystemResourceAsStream("img/folder.png"))));
+                        parentItem.getChildren().add(directoryNode);
+                    }
+                }
+            }
+        }
+    }
     @Override
     public void CreateTableView(TableView<Fileinfo> tableView, TableColumn<Fileinfo, String> nameColumn, TableColumn<Fileinfo, String> sizeColumn, TableColumn<Fileinfo, String> dateColumn, TableColumn<Fileinfo, ImageView> imageColumn) {
 
