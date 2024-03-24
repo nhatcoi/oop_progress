36bb56f6b790fe7685528c45b478ca77c82ee304
ndviet0303 <ndviet.work@gmail.com>
2024-03-25 00:11:21 +0700

    Continue Code: Add form create

--- src/main/java/com/mycompany/app/final_project/controllers/HomeController.java
+++ src/main/java/com/mycompany/app/final_project/controllers/HomeController.java
@@ -1,12 +1,15 @@
 package com.mycompany.app.final_project.controllers;
 
 import com.mycompany.app.App;
+import com.mycompany.app.final_project.models.Room;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +18,13 @@ import java.util.Objects;
 public class HomeController {
     @FXML
     public Button createBtn;
+    @FXML
+    public TableView<Room> tableRoom;
+
+    public void initialize() {
+        ObservableList<Room> rooms = Room.getRooms();
+        tableRoom.setItems(rooms);
+    }
 
     @FXML
     public void Create(ActionEvent actionEvent) throws IOException {
@@ -22,7 +32,7 @@ public class HomeController {
 
         Scene scene = new Scene(root, 640, 320);
         Stage stage = new Stage();
-        stage.setTitle("Create Window");
+        stage.setTitle("Create Room");
         stage.setScene(scene);
         stage.show();
     }
