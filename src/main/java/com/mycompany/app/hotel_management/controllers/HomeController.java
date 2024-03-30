6aa1898bd5b9e5959c84616e917e26ec69d222b5
ndviet0303 <ndviet.dev@gmail.com>
2024-03-30 23:32:13 +0700

    Add Room

--- src/main/java/com/mycompany/app/hotel_management/controllers/HomeController.java
+++ src/main/java/com/mycompany/app/hotel_management/controllers/HomeController.java
@@ -1,21 +1,29 @@
 package com.mycompany.app.hotel_management.controllers;
 
+import com.mycompany.app.hotel_management.entities.Room;
+import com.mycompany.app.hotel_management.enums.RoomStatus;
 import com.mycompany.app.hotel_management.enums.RoomType;
+import com.mycompany.app.hotel_management.repositories.database;
 import com.mycompany.app.hotel_management.utils.ToolFXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ResourceBundle;
+
+import com.mycompany.app.hotel_management.utils.Dialog;
 
 public class HomeController {
     @FXML
@@ -23,6 +31,8 @@ public class HomeController {
     public ComboBox<String> cbTypeRoom;
     public TextField rnameField;
     public TextField rpriceField;
+    public TableView<Room> tableView;
+    public Button btnAdd;
     @FXML
     private AnchorPane paneHome;
     @FXML
@@ -43,8 +53,10 @@ public class HomeController {
     private Button btnGuest;
     @FXML
     private Button btnUs;
+    private Connection connect;
+    private ObservableList<Room> roomList = FXCollections.observableArrayList();
 
-    public void initialize() {
+    public void initialize() throws SQLException {
         for (RoomType value : RoomType.values()) {
             cbTypeRoom.getItems().add(value.getText());
         }
@@ -52,7 +64,29 @@ public class HomeController {
         if (!cbTypeRoom.getItems().isEmpty()) {
             cbTypeRoom.setValue(cbTypeRoom.getItems().get(0));
         }
+
+        tableView.setItems(roomList);
+        fetchDataFromDatabase();
     }
+
+    private void fetchDataFromDatabase() throws SQLException {
+
+        connect = database.connectDb();
+        String query = "SELECT * FROM rooms";
+        assert connect != null;
+        ResultSet resultSet = connect.createStatement().executeQuery(query);
+        roomList.clear();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String type = RoomType.values()[resultSet.getInt("type")].getText();
+            String status = RoomStatus.values()[resultSet.getInt("status")].getText();
+            double price = resultSet.getDouble("price");
+
+            roomList.add(new Room(id, name, type, status, price));
+        }
+    }
+
     @FXML
     void signOut() throws IOException {
         ToolFXML.openFXML("views/authForm.fxml", 600, 400);
@@ -61,35 +95,35 @@ public class HomeController {
 
     @FXML
     void control(ActionEvent event) {
-        if(event.getSource() == this.btnControl) {
+        if (event.getSource() == this.btnControl) {
             show(control);
         }
     }
 
     @FXML
     void edit(ActionEvent event) {
-        if(event.getSource() == this.btnEdit) {
+        if (event.getSource() == this.btnEdit) {
             show(edit);
         }
     }
 
     @FXML
     void guests(ActionEvent event) {
-        if(event.getSource() == this.btnGuest) {
+        if (event.getSource() == this.btnGuest) {
             show(guests);
         }
     }
 
     @FXML
     void status(ActionEvent event) {
-        if(event.getSource() == this.btnStatus) {
+        if (event.getSource() == this.btnStatus) {
             show(status);
         }
     }
 
     @FXML
     void aboutUs(ActionEvent event) {
-        if(event.getSource() == this.btnUs) {
+        if (event.getSource() == this.btnUs) {
             try {
                 // open link browser
                 URI uri = new URI("https://github.com/nhatcoi/project_java");
@@ -119,6 +153,28 @@ public class HomeController {
     public void clearInput() {
         rnameField.clear();
         rpriceField.clear();
+
+    }
+
+    public void addData(ActionEvent actionEvent) {
+        connect = database.connectDb();
+        String name = rnameField.getText();
+        int type = cbTypeRoom.getSelectionModel().getSelectedIndex();
+        try {
+            double price = Double.parseDouble(rpriceField.getText());
+            String query = "INSERT INTO rooms (name, type, status, price) VALUES ('" + name + "', '" + type + "', 0, " + price + ")";
+            try {
+                assert connect != null;
+                connect.createStatement().executeUpdate(query);
+                fetchDataFromDatabase();
+                clearInput();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } catch (NumberFormatException e) {
+            Dialog.showError("Error", "Error", "Price Phải là số!");
+            return;
+        }
 
     }
 }
--- src/main/java/com/mycompany/app/hotel_management/entities/Room.java
+++ src/main/java/com/mycompany/app/hotel_management/entities/Room.java
@@ -2,51 +2,39 @@ package com.mycompany.app.hotel_management.entities;
 
 public class Room {
     private int id;
+    private String name;
     private String type;
     private String status;
     private double price;
 
-    // Constructors
-    public Room() {
-    }
+    // Constructors, getters and setters for other fields...
 
-    public Room(String type, String status, double price) {
+    // Constructor with name parameter
+    public Room(int id, String name, String type, String status, double price) {
+        this.id = id;
+        this.name = name;
         this.type = type;
         this.status = status;
         this.price = price;
     }
 
-    // Getters and setters
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getName() {
+        return name;
     }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getStatus() {
         return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public double getPrice() {
         return price;
     }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
 }
