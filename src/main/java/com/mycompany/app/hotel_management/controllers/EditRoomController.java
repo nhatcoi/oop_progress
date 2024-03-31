d771a755b0e7e75a8c55262f7bc709052c3c9e82
ndviet0303 <ndviet.work@gmail.com>
2024-04-01 04:31:20 +0700

    handle tableview click

--- src/main/java/com/mycompany/app/hotel_management/controllers/EditRoomController.java
+++ src/main/java/com/mycompany/app/hotel_management/controllers/EditRoomController.java
@@ -8,10 +8,12 @@ import com.mycompany.app.hotel_management.utils.Dialog;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -37,6 +39,21 @@ public class EditRoomController {
         }
         tableView.setItems(roomList);
         fetchDataFromDatabase();
+
+
+        tableView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                //if (event.getClickCount() == 2) { // Double click
+                    Room room = tableView.getSelectionModel().getSelectedItem();
+                    if (room != null) {
+                        rnameField.setText(room.getName());
+                        cbTypeRoom.setValue(room.getType());
+                        rpriceField.setText(String.valueOf(room.getPrice()));
+                    }
+                //}
+            }
+        });
     }
     public void clearInput() {
         rnameField.clear();
